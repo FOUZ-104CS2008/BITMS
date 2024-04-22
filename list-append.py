492dfbc93025e381386800aa676a0abd500889b8
FOUZ-104CS2008 <110445840+FOUZ-104CS2008@users.noreply.github.com>
2024-04-22 20:05:01 +0530

    Add files via upload

---
+++ list-append.py
@@ -0,0 +1,7 @@
+fouziya=[10,60,30,40]
+b=[]
+for i in fouziya:
+    if i not in b:
+        b.append(i)
+print(b)
+        
---
+++ listmethod.py
@@ -0,0 +1,12 @@
+fouziya=[10,20,30,40,50]
+b=[60,70,80,90,100]
+name=fouziya+b
+print(name)
+print(type(b))
+print(type(fouziya))
+print(name*2)
+print(len(name))
+print(max(name))
+print(min(name))
+
+
